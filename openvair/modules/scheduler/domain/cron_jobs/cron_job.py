25feada3c13804dca464380e64a7076dadeb31fc
Freya Pines <one-true-cat@yandex.ru>
2025-10-17 11:58:13 +0300

    add CronJobScheduler scaffolding

--- openvair/modules/scheduler/domain/cron_jobs/cron_job.py
+++ openvair/modules/scheduler/domain/cron_jobs/cron_job.py
@@ -3,3 +3,54 @@
 This module defines the `CronJobScheduler` concrete class that allows for
 management of cron jobs
 """
+
+from typing import Dict, Any
+
+from openvair.libs.log import get_logger
+from openvair.modules.scheduler.domain.base import BaseScheduler
+from openvair.modules.scheduler.domain.exception import SchedulerDomainException
+
+from crontab import CronTab
+
+LOG = get_logger(__name__)
+
+
+class CronJobScheduler(BaseScheduler):
+    def __init__(self, cron_obj: CronTab) -> None:
+        super().__init__(cron_obj)
+
+    def create(self, creation_data: Dict) -> None:
+        try:
+            with self._cron as cron:
+                for k, data in creation_data.items():
+                    job = cron.new(
+                        data.get('before'),
+                    )
+                    self._upd_job(job, data)
+                    self.jobs[k] = job
+
+        except SchedulerDomainException as error:
+            LOG.error(f'Failed to create a scheduled task: {error}')
+            raise
+
+    def edit(self, editing_data: Dict) -> Dict[str, Any]:
+        try:
+            with self._cron:
+                for k, upd_data in editing_data:
+                    job = self._job(k)
+                    self._upd_job(job, upd_data)
+            # TODO determine return values
+            return {}
+        except SchedulerDomainException as error:
+            LOG.error(f'Failed to edit scheduled tasks: {error}')
+            raise
+
+    def delete(self, key: str) -> None:
+        try:
+            job = self._job(key)
+            with self._cron as cron:
+                cron.remove(job)
+            self.jobs[key].delete()
+        except SchedulerDomainException as error:
+            LOG.error(f'Failed to delete scheduled task: {error}')
+            raise
