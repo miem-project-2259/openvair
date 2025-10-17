8f64602b24a3bf70497357d448f8918e16c31b53
Freya Pines <one-true-cat@yandex.ru>
2025-10-17 10:45:02 +0300

    added basic impl for scheduler server

--- openvair/modules/scheduler/domain/manager.py
+++ openvair/modules/scheduler/domain/manager.py
@@ -7,3 +7,17 @@ It creates an RPC server to consume messages related to scheduler management.
 
 #TODO dependencies
 """
+
+from openvair.libs.log import get_logger
+from openvair.libs.messaging.messaging_agents import MessagingServer
+
+LOG = get_logger('domain-manager')
+
+if __name__ == '__main__':
+    LOG.info('Starting RPCServer for consuming')
+    server = MessagingServer(
+        # TODO move this to a config
+        queue_name='scheduler_service_layer_domain',
+        manager=model.SchedulerFactory()
+    )
+    server.start
